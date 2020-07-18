f4de121abdec673d38ffb5e3081b9f88e6ba41af
Sho Watari <53390550+sho-watari@users.noreply.github.com>
2020-07-18 17:52:45 +0900

    Update logistic_regression.py

--- Supervised/logistic_regression.py
+++ Supervised/logistic_regression.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-
 from sklearn.datasets import load_breast_cancer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, plot_confusion_matrix, plot_precision_recall_curve, plot_roc_curve
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -13,29 +11,29 @@ if __name__ == "__main__":
     #
     breast = load_breast_cancer()
 
-    x_train, x_test, t_train, t_test = train_test_split(breast.data, breast.target, test_size=0.2, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(breast.data, breast.target, test_size=0.2, random_state=0)
 
     #
     # preprocessing
     #
     std = StandardScaler()
-    std.fit(x_train)
+    std.fit(X_train)
 
-    x_train_std = std.transform(x_train)
-    x_test_std = std.transform(x_test)
+    X_train_std = std.transform(X_train)
+    X_test_std = std.transform(X_test)
 
     #
     # logistic regression
     #
     clf = LogisticRegression(penalty="l2", solver="lbfgs", max_iter=100)
-    clf.fit(x_train_std, t_train)
+    clf.fit(X_train_std, y_train)
 
-    t_pred = clf.predict(x_test_std)
+    y_pred = clf.predict(X_test_std)
 
     #
     # performance evaluation
     #
-    tn, fp, fn, tp = confusion_matrix(t_test, t_pred).ravel()
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 
     accuracy = (tp + tn) / (tp + fp + fn + tn)
     precision = tp / (tp + fp)
@@ -50,13 +48,4 @@ if __name__ == "__main__":
     print("Specificity, True negative rate(TNR) {:.2f}%".format(specificity * 100))
     print("Negative predictive value(NPV) {:.2f}%".format(npv * 100))
     print("F-Score {:.2f}%".format(f_score * 100))
-
-    #
-    # visualization
-    #
-    plot_confusion_matrix(clf, x_test_std, t_test, cmap="hot")
-    plot_precision_recall_curve(clf, x_test_std, t_test)
-    plot_roc_curve(clf, x_test_std, t_test)
-
-    plt.show()
     
