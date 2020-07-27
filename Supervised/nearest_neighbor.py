43df23d393948907810aa547eaf2d57c78d02433
Sho Watari <53390550+sho-watari@users.noreply.github.com>
2020-07-27 22:58:15 +0900

    Update nearest_neighbor.py

--- Supervised/nearest_neighbor.py
+++ Supervised/nearest_neighbor.py
@@ -28,15 +28,18 @@ if __name__ == "__main__":
     #
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
     zz = np.hstack((xx.reshape(-1, 1), yy.reshape(-1, 1)))
+    
     z = clf.predict(zz).reshape(xx.shape)
 
-    plt.figure(figsize=(8, 6))
-    plt.pcolormesh(xx, yy, z, cmap=ListedColormap(["#1f77b4", "#2ca02c", "#ff7f0e"]))
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=ListedColormap(["#1f77b4", "#2ca02c", "#ff7f0e"]), edgecolor="k", s=20)
-    plt.title("k-Nearest Neighbor Classification")
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.pcolormesh(xx, yy, z, cmap=ListedColormap(["#1f77b4", "#2ca02c", "#ff7f0e"]))
+    ax.scatter(X[:, 0], X[:, 1], c=y, cmap=ListedColormap(["#1f77b4", "#2ca02c", "#ff7f0e"]), edgecolor="k", s=20)
+    ax.set_title("k-Nearest Neighbor Classification")
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    
     plt.show()
     
