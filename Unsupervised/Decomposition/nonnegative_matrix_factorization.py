b03aa0e3797c159e77f8c44ebd4ecf6968cf0a47
Sho Watarai <53390550+sho-watarai@users.noreply.github.com>
2020-12-18 20:55:39 +0900

    Update nonnegative_matrix_factorization.py

--- Unsupervised/Decomposition/nonnegative_matrix_factorization.py
+++ Unsupervised/Decomposition/nonnegative_matrix_factorization.py
@@ -12,7 +12,7 @@ img_height = 64
 img_width = 64
 
 
-if __name__ == "__main__":
+def nmf_face():
     file_list = glob.glob("../../../GAN/DCGAN/faces/*.jpg")
 
     faces = np.zeros((len(file_list), img_height, img_width), dtype="float32")
@@ -38,4 +38,35 @@ if __name__ == "__main__":
         plt.imshow(data[i], cmap="gray")
         plt.title("%d component" % (i + 1))
     plt.show()
-    
+
+
+def nmf_reconstruction_error():
+    file_list = glob.glob("../../../GAN/DCGAN/faces/*.jpg")
+
+    faces = np.zeros((len(file_list), img_height, img_width), dtype="float32")
+    for i, file in enumerate(file_list):
+        faces[i] = cv2.resize(cv2.imread(file, 0), (img_width, img_height))
+
+    #
+    # reconstruction error
+    #
+    errors = []
+    for i in range(1, 51):
+        nmf = NMF(n_components=i, init="random", max_iter=10000, random_state=0)
+        nmf.fit(faces.reshape(len(faces), -1))
+
+        errors.append(nmf.reconstruction_err_)
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(1, 51), errors, marker="o")
+    plt.xticks([1, 10, 20, 30, 40, 50], [1, 10, 20, 30, 40, 50])
+    plt.title("NMF Reconstruction Error")
+    plt.xlabel("number of components")
+    plt.ylabel("reconstruction error")
+    plt.show()
+
+
+if __name__ == "__main__":
+    nmf_face()
+
+    nmf_reconstruction_error()
