8da0916507d15b55c6113373de190053d11b82f0
wsteams <53390550+wsteams@users.noreply.github.com>
2021-04-23 19:33:10 +0900

    Update t-stochastic_neighbor_embedding.py

--- Unsupervised/Manifold/t-stochastic_neighbor_embedding.py
+++ Unsupervised/Manifold/t-stochastic_neighbor_embedding.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     plt.figure(figsize=(8, 6))
     for i, l in zip(np.arange(10), y):
         plt.scatter(X_tsne[y == i, 0], X_tsne[y == i, 1])
-    for i, l, w, h in zip(np.arange(10), y, X_tsne[:, 0], X_tsne[:, 1]):
+    for i, w, h in zip(np.arange(10), X_tsne[:, 0], X_tsne[:, 1]):
         plt.annotate(i, xy=(w + 0.001, h + 0.001), xytext=(0, 0), textcoords="offset points", ha="right", va="bottom")
     plt.title("t-distribution Stochastic Neighbor Embedding")
     plt.show()
